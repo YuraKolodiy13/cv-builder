a1d8a3672fcff9dd7d56e12687297b5e48bfb02c
YuraKolodiy13 <timber2017@gmail.com>
2023-05-02 19:58:32 +0200

    improve confirm modal

--- client/src/components/modals/ConfirmModal.tsx
+++ client/src/components/modals/ConfirmModal.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
+import Backdrop from "@mui/material/Backdrop";
+import Slide from "@mui/material/Slide";
 
 interface IConfirmModalProps {
   open: boolean;
@@ -21,15 +23,26 @@ const ConfirmModal: React.FC<IConfirmModalProps> = (props) => {
   };
 
   return (
-    <Modal open={open} onClose={onBackgroundClick} className='modal confirm-modal'>
-      <div className='modal__content'>
-        <h3>{modalTitle}</h3>
-        {children}
-        <div className="modal__btns">
-          <Button onClick={onBackgroundClick} variant="outlined">Cancel</Button>
-          <Button disabled={cvName === ''} onClick={handleSubmit} variant="contained">Confirm</Button>
+    <Modal
+      open={open}
+      onClose={onBackgroundClick}
+      className='modal confirm-modal'
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+    >
+      <Slide direction="down" in={open}>
+        <div className='modal__content'>
+          <h3>{modalTitle}</h3>
+          {children}
+          <div className="modal__btns">
+            <Button onClick={onBackgroundClick} variant="outlined">Cancel</Button>
+            <Button disabled={cvName === ''} onClick={handleSubmit} variant="contained">Confirm</Button>
+          </div>
         </div>
-      </div>
+      </Slide>
     </Modal>
   )
 };
