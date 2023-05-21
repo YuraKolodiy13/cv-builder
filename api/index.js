b9bdc68fd97efed6539a5d76a44cd93c4b1fdf5e
YuraKolodiy13 <timber2017@gmail.com>
2023-05-21 19:34:30 +0200

    revert routes updates to fix Error: No more than 12 Serverless Functions can be added to a Deployment on the Hobby plan

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRouter = require('./routers/auth');
+const cvRouter = require('./routers/cv');
+const commonRouter = require('./routers/common');
 const cors = require("cors");
+const authMiddleware = require('./middlewares/Auth')
 const cookieParser = require('cookie-parser')
-const router = require('./routers/index');
 
 require('dotenv').config();
 
@@ -20,7 +23,10 @@ app.use(cors(corsSettings));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", router);
+app.use("/api/auth", authRouter);
+app.use("/api/cv", authMiddleware, cvRouter);
+app.use("/api/common", commonRouter);
+
 
 const start = async () => {
   try {
