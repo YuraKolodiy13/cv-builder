832b44b0debd3091aa77392eb0a58ebca46baa4b
YuraKolodiy13 <timber2017@gmail.com>
2023-06-06 22:01:21 +0200

    remove file to avoid limit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRouter = require('./routers/auth');
+const cvRouter = require('./routers/cv');
+const commonRouter = require('./routers/common');
 const cors = require("cors");
+const authMiddleware = require('./middlewares/Auth')
 const cookieParser = require('cookie-parser')
-const router = require('./routers/index');
 
 require('dotenv').config();
 
@@ -20,14 +23,14 @@ app.use(cors(corsSettings));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", router);
+app.use("/api/auth", authRouter);
+app.use("/api/cv", authMiddleware, cvRouter);
+app.use("/api/common", commonRouter);
+
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (e) {
     console.log(e)
